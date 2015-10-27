b9961cbb7a0312dc271879e65a990d9e5b608005
Alex <mrclean.durrant@gmail.com>
2015-10-26 18:14:54 -0600

    Initial Commit

---
+++ GameArchiveLib/GameArchive.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameArchiveLib
+{
+
+    public sealed class GameArchive
+    {
+
+        private static readonly GameArchive instance = new GameArchive();
+
+        static GameArchive()
+        {
+            
+        }
+
+        private GameArchive()
+        {
+            
+        }
+
+        public static GameArchive Instance
+        {
+            get { return instance;}
+        }
+
+        public void SaveData<DataType>(DataType gameData, string filePath)
+        {
+            FileStream stream = File.Create(filePath);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, gameData);
+            stream.Close();
+        }
+
+        public DataType LoadData<DataType>(string filePath)
+        {
+            FileStream stream = File.OpenRead(filePath);
+            BinaryFormatter formatter = new BinaryFormatter();
+            DataType returnData = (DataType)formatter.Deserialize(stream);
+            stream.Close();
+            return returnData;
+        }
+    }
+
+    
+}
---
+++ GameArchiveTests/GameArchiveTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameArchiveLib;
+using GameDataLib;
+using GameObjectLib;
+using GameObserverLib;
+using NUnit.Framework;
+
+namespace GameArchiveTests
+{
+    [TestFixture]
+    public class GameArchiveTests
+    {
+        [TestCase(TestName = "Saving GameData via SaveData() method works correctly")]
+        public void SaveDataToDrive()
+        {
+            GameObject gameObject = new GameObject("new game object");
+            Property newProperty = new Property(PropertyType.String, "new property", "new value");
+            gameObject.AddProperty(newProperty);
+            GameObserver Observer = GameObserver.Instance;
+            Observer.ObserveGameObject(gameObject, ObjectStatus.Active);
+            GameData gameData = new GameData(Observer.GetObserverData());
+            GameArchive Archive = GameArchive.Instance;
+            Archive.SaveData(gameData, "C:\\GameCraftData\\testgamedata.gcd");
+            Assert.IsTrue(true);
+        }
+
+        [TestCase(TestName = "Loading GameData via LoadData() method works correctly")]
+        public void LoadDataFromDrive()
+        {
+            GameObject gameObject = new GameObject("new game object");
+            Property newProperty = new Property(PropertyType.String, "new property", "new value");
+            gameObject.AddProperty(newProperty);
+            GameObserver Observer = GameObserver.Instance;
+            Observer.ObserveGameObject(gameObject, ObjectStatus.Active);
+            GameData gameData = new GameData(Observer.GetObserverData()); 
+            GameArchive Archive = GameArchive.Instance;
+            Archive.SaveData(gameData,"C:\\GameCraftData\\testgamedata.gcd");
+
+            GameData loadData = Archive.LoadData <GameData>("C:\\GameCraftData\\testgamedata.gcd");
+            Assert.IsTrue(loadData.ObserverData.ActiveObjects["new game object"].Name == gameObject.Name);
+        }
+    }
+}
